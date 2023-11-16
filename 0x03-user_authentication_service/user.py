a350cd19be9d138af276e84d1b06c5579e24b25c
seb-art <kimanzikims300@gmail.com>
2023-11-16 20:45:10 +0000

    tasks

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -4,11 +4,14 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = 'users'
 
+class User(Base):
+    """Represents a record from the `user` table.
+    """
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
